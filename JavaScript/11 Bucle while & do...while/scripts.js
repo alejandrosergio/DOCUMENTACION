c7eecd3b95dbbd3d354a9a9ba15c111a0bb523c1
alejandrosergio <alejosergio3000@gmail.com>
2021-01-12 01:00:48 -0500

    elaborando ejercicios del 1 al 5 terminado

--- JavaScript/11 Bucle while & do...while/scripts.js
+++ JavaScript/11 Bucle while & do...while/scripts.js
@@ -14,13 +14,13 @@
 
 let pass = '';
 
-// El bucle no entra si se cumple la condicion, como vemos pass es igual a 'hola' y la condicion es si es distinto. se ejecutara una y otra vez hasta que se cumpla esa condicion si se cumple desde un principio no se ejecutara el bucle
+
 while(pass != 'hola'){
     pass = prompt('Introduzca su contraseña')
 }
 console.log('Fin del bucle');
 
-//El bucle se ejecutara una vez al menos, ya que la condicion esta al final, se ejecutara mientras su condicion sea lo contrario.
+
 do{
     pass = prompt('Introduzca su contraseña')
 
