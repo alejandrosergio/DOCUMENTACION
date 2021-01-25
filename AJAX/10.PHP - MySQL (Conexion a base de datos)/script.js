198b5ee0d2ef0ad0ec9bab90b305d9afa0ae9cd0
alejandrosergio <alejosergio3000@gmail.com>
2021-01-25 15:12:30 -0500

    Curso de AJAX del video 6 al 10

---
+++ AJAX/10.PHP - MySQL (Conexion a base de datos)/script.js
@@ -0,0 +1,22 @@
+let resultado = document.getElementById("info");
+
+function mostrarUsuarios()
+{
+    let xmlhttp;
+
+    //Verificando el tipo de navagador con el que contamos
+    if(window.XMLHttpRequest){
+        xmlhttp = new XMLHttpRequest();
+    }else{
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+
+        xmlhttp.onreadystatechange = function(){
+            if (xmlhttp.readyState === 4 && xmlhttp.status == 200){
+                resultado.innerHTML = this.responseText;
+            }   
+
+        xmlhttp.open("GET","servidor.php",true);
+        xmlhttp.send(); 
+    }
+}
