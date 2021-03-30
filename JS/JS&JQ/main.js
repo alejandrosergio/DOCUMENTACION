116013126dc13b9cb60fcd340b2120035a0bad99
alejandrosergio <alejosergio3000@gmail.com>
2021-03-30 18:53:13 -0500

    primeros pasos

--- JS/JS&JQ/main.js
+++ JS/JS&JQ/main.js
@@ -1,6 +1,32 @@
-'use strict';
+'use strict'; // Para llevar unas buenas practicas en el JS
 
-let num = 0 ;
+/* Diferencia entre var y let */
+
+/* var mantiene el valor de su variable y la podemos cambiar por un bloque con let y pueda seguir con su contenido principal */
+
+var num = 25;
 
 console.log(num);
-alert("Hello Word");
+
+if (true) {
+    let num = 20;
+    console.log(num);
+}
+
+console.log(num);
+
+if(!(num > 25 && num < 30)){
+    alert("esta en el rango");
+}else{
+    alert("no esta en el rango");
+}
+
+if(num > 25 && num < 30){
+    alert("esta en el rango");
+}else{
+    alert("no esta en el rango");
+}
+
+for (let i = 0; i <= 100; i++) {
+        console.log("N:" +i);
+}
