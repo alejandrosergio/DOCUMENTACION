9ceeed9733790e057a8e952912dc85992f4f0839
AlejandroSergio <alejosergio3000@gmail.com>
2021-06-22 13:05:47 -0500

    vamos

---
+++ ANGULAR/ANGULAR/02-gifsApp/src/app/gifs/busqueda/busqueda.component.ts
@@ -0,0 +1,24 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-busqueda',
+  templateUrl: './busqueda.component.html',
+  styles: [
+  ]
+})
+export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+
+  bucar(){
+
+    const valor = this.txtBuscar.nativeElement.value;
+
+    console.log(valor);
+
+    this.txtBuscar.nativeElement.value = '';
+
+  }
+
+
+}
