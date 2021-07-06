50e78da746e2145b631e45a1e9fb7d47471569be
AlejandroSergio <alejosergio3000@gmail.com>
2021-07-06 15:53:23 -0500

    a 4 videos

--- ANGULAR/ANGULAR/05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
+++ ANGULAR/ANGULAR/05-heroesApp/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { switchMap } from "rxjs/operators";
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [`
+    img{
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class AgregarComponent implements OnInit {
 
@@ -30,20 +37,51 @@ export class AgregarComponent implements OnInit {
     alt_img: ''
   }
 
-  constructor( private heroeService: HeroesService ) { }
+  constructor( 
+    private heroeService: HeroesService,
+    private activateRouter: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit(): void {
+
+    if ( !this.router.url.includes( 'editar' )) {
+      return;
+    }
+
+    this.activateRouter.params
+    .pipe(
+      switchMap( ({ id }) => this.heroeService.getHeroePorId( id ) )
+    )
+    .subscribe( heroe => this.heroe = heroe );
   }
 
   guardar(){
     if ( this.heroe.superhero.trim().length === 0) {
       return;
     }
+
+    if (this.heroe.id) {
+      this.heroeService.actualizarHeroe( this.heroe)
+      .subscribe( heroe => {
+        this.router.navigate(['/heroes', heroe.id ]);
+      });
+    } else {
+      this.heroeService.agregarHeroe( this.heroe )
+      .subscribe( heroe =>   {
+        this.router.navigate(['/heroes/listado']);
+      });
+    }
     
-    this.heroeService.agregarHeroe( this.heroe )
-    .subscribe( resp =>   {
-      console.log('Resp= ', resp)
+  }
+
+  borrar(){
+
+    this.heroeService.eliminarHeroe( this.heroe.id! )
+    .subscribe( resp => {
+      this.router.navigate(['/heroes/listado']);
     });
+
   }
 
 }
