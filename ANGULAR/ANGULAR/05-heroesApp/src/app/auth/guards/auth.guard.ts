eac1aed61f3cc7123b8394b999246c1c5dcdd6ab
AlejandroSergio <alejosergio3000@gmail.com>
2021-07-07 17:45:11 -0500

    hola mundo

---
+++ ANGULAR/ANGULAR/05-heroesApp/src/app/auth/guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanLoad, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanLoad, CanActivate {
+
+  constructor( private authService: AuthService ){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+      if ( this.authService.auth.id ) {
+        return true;
+      }else{
+        return false;
+      }
+  }
+
+  canLoad(
+    route: Route,
+    segments: UrlSegment[]): Observable<boolean> | Promise<boolean> | boolean {
+
+      if ( this.authService.auth.id ) {
+        return true;
+      }else{
+        return false;
+      }
+  }
+}
