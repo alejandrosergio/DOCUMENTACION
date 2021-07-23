77d1ca5c3097b18d3eb00b8a1d4519ba3b03097d
AlejandroSergio <alejosergio3000@gmail.com>
2021-07-23 10:38:01 -0500

    make it better

--- ANGULAR/ANGULAR/12-authAPP/fern/12-auth-server/index.js
+++ ANGULAR/ANGULAR/12-authAPP/fern/12-auth-server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+
 const { dbConnection } = require('./db/config');
 require('dotenv').config();
 
@@ -24,7 +26,12 @@ app.use( express.json() );
 app.use( '/api/auth', require('./routes/auth') );
 
 
+// Manejar las demas rutas
+app.get( '*', (req , res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html') );
+});
+
+
 app.listen( process.env.PORT, () => {
     console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
 });
-
