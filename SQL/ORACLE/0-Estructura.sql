7d767e9cfb9558fc4470bf6c1a3525ab93eca3af
ALEJANDRO seeik <alejandroseeik@MacBook-Air-de-Sergiio.local>
2021-07-26 17:13:45 -0500

    um

---
+++ SQL/ORACLE/0-Estructura.sql
@@ -0,0 +1,20 @@
+/* ESTRUCTURA ORACLE */
+
+SET SERVEROUTPUT ON;
+-- Permitir salidas por consola ESTANDAR -- varia su asignación
+
+
+DECLARE
+-- Definición de funciones y variables
+
+
+
+BEGIN
+-- Inicio de una sentencia
+
+
+
+
+
+END;
+-- Final de una sentencia
