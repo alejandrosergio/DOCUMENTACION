03e9383d19ec845ee1c810a5f882fe2a89435589
ALEJANDRO seeik <alejandroseeik@MacBook-Air-de-Sergiio.local>
2021-08-02 10:29:11 -0500

    seccion 1 del curso de spring terminado

---
+++ SPRING/HolaMundo/src/main/java/com/example/holamundo/HolaMundoApplication.java
@@ -0,0 +1,13 @@
+package com.example.holamundo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication  //El @texto una anotación.
+public class HolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HolaMundoApplication.class, args);
+	}
+
+}
