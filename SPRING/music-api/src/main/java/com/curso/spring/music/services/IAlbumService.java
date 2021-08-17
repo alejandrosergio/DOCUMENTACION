b0d9309867622ddf6a2db4eadc9cee191597445b
ALEJANDRO seeik <alejandroseeik@MacBook-Air-de-Sergiio.local>
2021-08-17 18:40:15 -0500

    vamos que vamos

---
+++ SPRING/music-api/src/main/java/com/curso/spring/music/services/IAlbumService.java
@@ -0,0 +1,21 @@
+package com.curso.spring.music.services;
+
+import java.util.List;
+
+import com.curso.spring.music.models.AlbumsModel;
+
+
+public interface IAlbumService {
+
+	// DEBEMOS OBTENER LOS REGISTROS DE ALBUNES EN LA BD
+	List<AlbumsModel> buscarAlbunes();
+	
+	
+	// DEBEMOS GUARDAR UN ALBUN EN LA BD
+	void guardarAlbum(AlbumsModel albumsModel);
+	
+	
+	// DEBEMOS ELIMINAR UN ALBUN EN LA BD
+	void eliminarAlbum(int idAlbum);
+	
+}
